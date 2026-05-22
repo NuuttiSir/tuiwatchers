1483cf5f07f35a4196699442050cf510f4676ee8
nuuttisir <nuutti.sirvio@gmail.com>
2026-05-22 11:14:29 +0300

    started working on refresh tokens

--- main.go
+++ main.go
@@ -18,9 +18,10 @@ type DeviceCodeResponse struct {
 }
 
 type AccessToken struct {
-	AccessToken string `json:"access_token"`
-	ExpiresIn   int    `json:"expires_in"`
-	TokenType   string `json:"token_type"`
+	AccessToken  string `json:"access_token"`
+	RefreshToken string `json:"refresh_token"`
+	ExpiresIn    int    `json:"expires_in"`
+	TokenType    string `json:"token_type"`
 }
 
 type UserData struct {
